5074eb250672fecbd9de9d0d32e33952093a05e2
Shubham Dev <devshubham22@gmail.com>
2024-05-23 19:12:12 +0530

    Update Exp  2.java

---
+++ Java/Exp  2.java
@@ -0,0 +1,2 @@
+Aim- (A) Write a Program to find whether the number is prime number or not using ‘for loop.
+      (B) Write a Program to find whether the number is prime number or not using ‘while loop’
