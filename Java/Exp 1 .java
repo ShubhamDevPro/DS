d6efa65b0232f57003e3f113be74a37d645588e6
Shubham Dev <devshubham22@gmail.com>
2024-05-24 07:15:16 +0530

    Update and rename Exp 1 .java to Exp 1 Hello World .java

--- Java/Exp 1 .java
+++
@@ -1,2 +0,0 @@
-Aim- (A) Write a Program to print “Hello World” using command prompt.
-  (B) Write a Program to calculate the area of rectangle using command prompt
---
+++ Java/Exp 1 Hello World .java
@@ -0,0 +1,17 @@
+Aim- (A) Write a Program to print “Hello World” using command prompt.
+  (B) Write a Program to calculate the area of rectangle using command prompt
+  
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+//------------------------------------------------------------
+public class Main{
+    public static void main(String[] args){
+        int length  = 25;
+        int breadth = 12;
+        int area = length * breadth;
+        System.out.println("Area of Rectangle is :"+ area);
+    }
+}
