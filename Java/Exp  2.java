c8f8997acb2275fe40c46e8e27c9e56c34ac5c96
Shubham Dev <devshubham22@gmail.com>
2024-05-24 07:07:47 +0530

    Update Exp  2.java

--- Java/Exp  2.java
+++ Java/Exp  2.java
@@ -1,2 +1,47 @@
 Aim- (A) Write a Program to find whether the number is prime number or not using ‘for loop.
       (B) Write a Program to find whether the number is prime number or not using ‘while loop’
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    int num = 29;
+    boolean flag = false;
+    for (int i = 2; i <= num / 2; ++i) {
+      // condition for nonprime number
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+    }
+
+    if (!flag)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+  }
+}
+
+//------------------------------------------------------------------
+public class Main {
+
+  public static void main(String[] args) {
+
+    int num = 33, i = 2;
+    boolean flag = false;
+    while (i <= num / 2) {
+      // condition for nonprime number
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+
+      ++i;
+    }
+
+    if (!flag)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+  }
+}
